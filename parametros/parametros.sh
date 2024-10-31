fc2b5543a5f224c0ccded4ed93ea7fde94025fed
Damian Piazza <83801199+XkaunanX@users.noreply.github.com>
2024-10-31 09:22:21 -0300

    Delete parametros directory

--- parametros/parametros.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Nombre del script: $0"
-echo "Primer argumento: $1"
-echo "Todos los argumentos (forma 1): $@"
-echo "Todos los argumentos (forma 2): $*"
-echo "Número de argumentos: $#"
-echo "PID del script actual: $$"
-echo "Codigo de salida del ultimo comando: $?"
